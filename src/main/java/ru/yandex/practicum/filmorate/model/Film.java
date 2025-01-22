a6adb66613661ee995ebe7ddc6da8c8c140697af
NikitaChernyaev777 <oth117@yandex.ru>
2025-01-22 12:20:01 +0300

    Сдача ТЗ проекта № 10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.validation.annotation.ReleaseDateConstraint;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+
+    private Long id;
+
+    @NotBlank(message = "Название фильма не может быть пустым!")
+    private String name;
+
+    @Size(max = 200, message = "Описание фильма не может занимать более 200 символов!")
+    private String description;
+
+    @ReleaseDateConstraint
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительным числом!")
+    private Integer duration;
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserControllerTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    public void setUp() {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    private User createValidUser() {
+        User user = new User();
+        user.setEmail("user@mail.ru");
+        user.setLogin("Login");
+        user.setName("User");
+        return user;
+    }
+
+    @Test
+    public void shouldPassWhenValidRequiredFields() {
+        User user = createValidUser();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(0, violations.size(),
+                "Ожидалось, что пользователь с корректными данными пройдет валидацию");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "invalid_email", "mail.ru"})
+    public void shouldFailWhenInvalidEmail(String email) {
+        User user = createValidUser();
+        user.setEmail(email);
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(),
+                "Некорректный электронный адрес '" + email + "' не должен проходить валидацию");
+    }
+
+    @Test
+    public void shouldFailWhenSpacesInLogin() {
+        User user = createValidUser();
+        user.setLogin("Log in");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(), "Логин с пробелами не должен проходить валидацию");
+    }
+
+    @Test
+    public void shouldFailWhenBirthDateInFuture() {
+        User user = createValidUser();
+        user.setBirthday(LocalDate.MAX);
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(1, violations.size(), "День рождения в будущем не должен проходить валидацию");
+    }
+}
